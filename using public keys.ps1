2282b9902ea3e16657b38cdb35d6a48307a189ad
pfdalfpc92-coder <pfdalfpc92@gmail.com>
2025-08-25 15:39:22 +0800

    Add files via upload

---
+++ using public keys.ps1
@@ -0,0 +1,12 @@
+﻿Implementing Data Confidentiality
+using Public Keys:
+@coreBABA:
+config t
+username admin privilege 15 secret pass
+username gen privilege 15 secret pass
+ip domain-name pfda.gov.ph
+crypto key generate rsa
+!! tanong anong KeySize: 256, 512, 1024
+ip ssh version 2
+ip ssh time-out 120
+end
